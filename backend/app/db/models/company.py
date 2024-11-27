7433cc11824220e98e117bcbe7b9c2e0bab8af07
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-27 18:19:43 +0300

    add company model

---
+++ backend/app/db/models/company.py
@@ -0,0 +1,24 @@
+from sqlalchemy import String, SMALLINT, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.models.mixins import UUIDPrimaryKey
+from app.db.models.base import Base
+
+
+class Company(Base, UUIDPrimaryKey):
+    """Модель компании."""
+
+    __tablename__ = "company"
+
+    name: Mapped[str] = mapped_column(comment="Наименование организации")
+    description: Mapped[str] = mapped_column(comment="Описание организации")
+    subdomain: Mapped[str] = mapped_column(String(50), comment="Субдомен организации")
+    timezone_id: Mapped[int] = mapped_column(
+        SMALLINT,
+        ForeignKey("timezones.id", ondelete="RESTRICT"),
+        comment="Идентификатор таймзоны",
+    )
+    logo: Mapped[str] = mapped_column(comment="Логотип организации")
+
+    def __repr__(self):
+        return self.name
