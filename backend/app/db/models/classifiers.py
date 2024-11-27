811b73b58fffa54ba1aae150eab62b5425ff36b8
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-27 18:56:57 +0300

    add roles classifier

--- backend/app/db/models/classifiers.py
+++ backend/app/db/models/classifiers.py
@@ -1,8 +1,10 @@
-from sqlalchemy import String
+import uuid
+
+from sqlalchemy import String, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.models import Base
-from app.db.models.mixins import SmallIntPrimaryKey
+from app.db.models.mixins import SmallIntPrimaryKey, UUIDPrimaryKey
 
 
 class Timezones(Base, SmallIntPrimaryKey):
@@ -12,3 +14,14 @@ class Timezones(Base, SmallIntPrimaryKey):
 
     display_name: Mapped[str] = mapped_column(String(30), comment="Отображаемое имя часового пояса")
     iana_name: Mapped[str] = mapped_column(String(256), comment="Наименование IANA часового пояса")
+
+
+class Roles(Base, UUIDPrimaryKey):
+    """Пополняемый классификатор ролей."""
+
+    __tablename__ = "roles"
+    __table_args__ = (UniqueConstraint("company_id", "name"),)
+    name: Mapped[str] = mapped_column(String(30), comment="Название роли")
+    company_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("companies.id"), comment="Идентификатор компании управляющей ролью"
+    )
