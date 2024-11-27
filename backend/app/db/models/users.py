e69e6c33beeb85e6d22a692377ac410a3110f4a4
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-27 18:03:38 +0300

    add docstring and __repr__ function

--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -6,22 +6,24 @@ from app.db.models.base import Base
 
 
 class User(Base, UUIDPrimaryKey):
+    """Модель пользователя"""
+
     __tablename__ = "users"
 
-    email: Mapped[str] = mapped_column(String(100), comment="Почта")
-    username: Mapped[str] = mapped_column(String(30), comment="Имя пользователя")
+    email: Mapped[str] = mapped_column(String(100), comment="Почта", unique=True)
+    username: Mapped[str] = mapped_column(String(30), comment="Имя пользователя", unique=True)
     hashed_password: Mapped[str] = mapped_column(comment="Хеш пароля")
     first_name: Mapped[str] = mapped_column(String(30), comment="Имя")
     second_name: Mapped[str] = mapped_column(String(30), comment="Фамилия")
     middle_name: Mapped[str] = mapped_column(String(30), comment="Отчество")
-    image: Mapped[str] = mapped_column(comment="Путь к файлу изображения пользователя")
+    image: Mapped[str] = mapped_column(comment="Путь к файлу изображения пользователя", unique=True)
     timezone_id: Mapped[int] = mapped_column(
         SMALLINT,
-        ForeignKey(
-            "timezones.id",
-            ondelete="RESTRICT",
-        ),
+        ForeignKey("timezones.id", ondelete="RESTRICT"),
         comment="Идентификатор таймзоны",
     )
     is_bot: Mapped[bool] = mapped_column(comment="Является ли пользователь ботом")
     active: Mapped[bool] = mapped_column(comment="Активен ли аккаунт пользователя")
+
+    def __repr__(self):
+        return self.username
