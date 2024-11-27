0d3d3cb9f94e4da6019579f7b0d5a319eee42164
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-27 17:59:58 +0300

    add user model

---
+++ backend/app/db/models/users.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, SMALLINT, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.models.mixins import UUIDPrimaryKey
+from app.db.models.base import Base
+
+
+class User(Base, UUIDPrimaryKey):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(String(100), comment="Почта")
+    username: Mapped[str] = mapped_column(String(30), comment="Имя пользователя")
+    hashed_password: Mapped[str] = mapped_column(comment="Хеш пароля")
+    first_name: Mapped[str] = mapped_column(String(30), comment="Имя")
+    second_name: Mapped[str] = mapped_column(String(30), comment="Фамилия")
+    middle_name: Mapped[str] = mapped_column(String(30), comment="Отчество")
+    image: Mapped[str] = mapped_column(comment="Путь к файлу изображения пользователя")
+    timezone_id: Mapped[int] = mapped_column(
+        SMALLINT,
+        ForeignKey(
+            "timezones.id",
+            ondelete="RESTRICT",
+        ),
+        comment="Идентификатор таймзоны",
+    )
+    is_bot: Mapped[bool] = mapped_column(comment="Является ли пользователь ботом")
+    active: Mapped[bool] = mapped_column(comment="Активен ли аккаунт пользователя")
