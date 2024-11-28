299134e773c8571429cd4a4e06a710d59b7bebea
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-28 17:59:33 +0300

    add task type model

--- backend/app/db/models/classifiers.py
+++ backend/app/db/models/classifiers.py
@@ -1,10 +1,16 @@
 import uuid
 
 from sqlalchemy import String, ForeignKey, UniqueConstraint
+from sqlalchemy.dialects.mysql import BIGINT
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.models.base import Base
-from app.db.models.mixins import SmallIntPrimaryKeyMixin, UUIDPrimaryKeyMixin, PermissionClassifierMixin
+from app.db.models.mixins import (
+    SmallIntPrimaryKeyMixin,
+    UUIDPrimaryKeyMixin,
+    PermissionClassifierMixin,
+    BigIntPrimaryKeyMixin,
+)
 
 
 class Timezones(Base, SmallIntPrimaryKeyMixin):
@@ -38,3 +44,22 @@ class ProjectPermission(Base, SmallIntPrimaryKeyMixin, PermissionClassifierMixin
     """Классификатор возможных разрешений для проекта."""
 
     __tablename__ = "project_permissions"
+
+
+class TaskType(Base, BigIntPrimaryKeyMixin):
+    """Пополняемый классификатор типов задач."""
+
+    __tablename__ = "task_types"
+    __table_args__ = {"constraints": (UniqueConstraint("name", "company_id", "project_id", name="uq_task_type_name"),)}
+
+    name: Mapped[str] = mapped_column(String(100), comment="Наименование типа задачи")
+    description: Mapped[str] = mapped_column(comment="Описание типа задачи")
+    icon_id: Mapped[int] = mapped_column(
+        BIGINT, ForeignKey("icons.id", ondelete="RESTRICT"), comment="Идентификатор иконки"
+    )
+    company_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("companies.id", ondelete="CASCADE"), comment="Идентификатор организации"
+    )
+    project_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("projects.id", ondelete="CASCADE"), comment="Идентификатор проекта"
+    )
