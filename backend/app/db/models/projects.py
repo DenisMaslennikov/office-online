0f8153bce529daaf6678f57b2b5fa0a214e4af20
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-28 12:26:33 +0300

    Add project model

---
+++ backend/app/db/models/projects.py
@@ -0,0 +1,19 @@
+import uuid
+
+from sqlalchemy import String, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.models.base import Base
+from app.db.models.mixins import UUIDPrimaryKeyMixin
+
+
+class Project(Base, UUIDPrimaryKeyMixin):
+    """Модель проекта."""
+
+    __tablename__ = "projects"
+    __table_args__ = {"constraints": (UniqueConstraint("name", "company_id", name="uq_project_name"),)}
+
+    name: Mapped[str] = mapped_column(String(255), comment="Название проекта")
+    prefix: Mapped[str] = mapped_column(String(50), comment="Префикс проекта")
+    description: Mapped[str | None] = mapped_column(comment="Описание проекта")
+    company_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("companies.id"), comment="Идентификатор организации")
