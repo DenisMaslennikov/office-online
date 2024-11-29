dbd2f76a576494f9cd9f706168d16c9aa151aa46
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-29 14:58:03 +0300

    add model with default permissions by file group type

--- backend/app/db/models/classifiers.py
+++ backend/app/db/models/classifiers.py
@@ -108,3 +108,17 @@ class FileGroupType(Base, SmallIntPrimaryKeyMixin):
     system_name: Mapped[str] = mapped_column(String(100), comment="Системное имя типа группы файлов", unique=True)
     system: Mapped[bool] = mapped_column(comment="Системная группа (нельзя менять права по умолчанию)")
     trash: Mapped[bool] = mapped_column(comment="Признак того что группа является корзиной")
+
+
+class FileGroupTypeDefaultPermission(Base, SmallIntPrimaryKeyMixin):
+    """Настройки прав по умолчанию для типов групп файлов."""
+
+    __tablename__ = "file_group_type_default_permissions"
+
+    role_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"), comment="Идентификатор роли")
+    file_group_type_id: Mapped[int] = mapped_column(
+        SMALLINT, ForeignKey("file_group_types.id", ondelete="CASCADE"), comment="Идентификатор типа группы файлов"
+    )
+    file_group_permission_id: Mapped[int] = mapped_column(
+        SMALLINT, ForeignKey("file_group_permissions.id", ondelete="CASCADE"), comment="Идентификатор разрешения"
+    )
