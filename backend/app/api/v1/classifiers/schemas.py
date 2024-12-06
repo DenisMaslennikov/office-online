c2555eb6ca370c50d6e3478095c8a77771baa2ae
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-06 18:16:48 +0300

    add model config

--- backend/app/api/v1/classifiers/schemas.py
+++ backend/app/api/v1/classifiers/schemas.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict
 
 
 class TimezoneReadSchema(BaseModel):
     """Схема сериализации таймзон для чтения."""
+
+    model_config = ConfigDict(from_attributes=True)
 
     id: int
     display_name: str
