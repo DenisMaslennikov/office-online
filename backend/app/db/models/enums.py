9b51a4f4c575023c1600ad8253e4c16a80e5cdbe
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-20 10:36:31 +0300

    add channel icon category

--- backend/app/db/models/enums.py
+++ backend/app/db/models/enums.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Enum
 
-icon_category_enum = Enum("TASK_TYPE", "FILE_GROUP", "PROJECT", name="icon_category")
+icon_category_enum = Enum("TASK_TYPE", "FILE_GROUP", "PROJECT", "CHANNEL", name="icon_category")
 
 subject_type_enum = Enum("ROLE", "USER", name="subject_type")
 
