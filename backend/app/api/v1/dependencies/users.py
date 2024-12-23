78855a472c69833da4bfe6dd9a56955b4f1c5fc8
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-23 21:05:27 +0300

    add caching

--- backend/app/api/v1/dependencies/users.py
+++ backend/app/api/v1/dependencies/users.py
@@ -5,13 +5,13 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from app.config import settings
 from app.api.v1.dependencies.jwt import get_current_user_id, get_user_id_from_refresh_token
 from app.api.v1.users import crud
-from app.api.v1.users.schemas import UserLoginSchema, UserResponseSchema, UserCashSchema
+from app.api.v1.users.schemas import UserCashSchema, UserLoginSchema
+from app.config import settings
 from app.db import db_helper
 from app.db.models import User
-from app.utils.redis import get_from_cash, update_cash
+from app.db.redis import get_from_cache, update_cache
 
 
 async def user_is_active(user: User | UserCashSchema) -> bool:
@@ -41,14 +41,14 @@ async def get_current_user(
     session: Annotated[AsyncSession, Depends(db_helper.get_session)],
 ) -> UserCashSchema:
     """Получает текущего пользователя."""
-    user_from_cash = await get_from_cash(settings.redis.user_prefix, user_id, UserCashSchema)
+    user_from_cash = await get_from_cache(settings.redis.user_prefix, user_id, UserCashSchema)
     if user_from_cash is not None:
         await user_is_active(user_from_cash)
         return user_from_cash
 
-    user_from_db = await crud.get_user_by_id_repo(session, user_id=user_id)
+    user_from_db = await crud.get_user_by_id_repo(session, user_id, joinedload(User.timezone))
     user = UserCashSchema.model_validate(user_from_db)
-    await update_cash(settings.redis.user_prefix, user)
+    await update_cache(settings.redis.user_prefix, user)
 
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Пользователь не найден")
@@ -59,10 +59,11 @@ async def get_current_user(
 async def get_user_from_refresh_token(
     user_id: Annotated[UUID, Depends(get_user_id_from_refresh_token)],
     session: Annotated[AsyncSession, Depends(db_helper.get_session)],
-) -> User:
+) -> User | UserCashSchema:
     """Получает объект пользователя из рефреш токена."""
-    user_from_cash = await get_from_cash(settings.redis.user_prefix, user_id, UserCashSchema)
+    user_from_cash = await get_from_cache(settings.redis.user_prefix, user_id, UserCashSchema)
     if user_from_cash is not None:
+        await user_is_active(user_from_cash)
         return user_from_cash
 
     user = await crud.get_user_by_id_repo(session, user_id=user_id)
