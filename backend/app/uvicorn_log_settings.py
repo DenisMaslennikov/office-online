8bdc3f1e3e66a76f6dea58efc7dc47620ae7dab4
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-29 14:33:26 +0300

    add uvicorn log settings

---
+++ backend/app/uvicorn_log_settings.py
@@ -0,0 +1,51 @@
+from app.config import settings
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "standard": {"format": settings.logs.uvicorn_standard_log_format},
+        "custom_formatter": {"format": settings.logs.uvicorn_custom_log_format},
+    },
+    "handlers": {
+        "default": {
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # Default is stderr
+        },
+        "stream_handler": {
+            "formatter": "custom_formatter",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # Default is stderr
+        },
+        "file_handler": {
+            "formatter": "custom_formatter",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": settings.logs.uvicorn_log_file,
+            "maxBytes": settings.logs.uvicorn_max_bytes,
+            "backupCount": settings.logs.uvicorn_backup_count,
+        },
+    },
+    "loggers": {
+        "uvicorn": {
+            "handlers": ["default", "file_handler"],
+            "level": settings.logs.uvicorn_log_level,
+            "propagate": False,
+        },
+        "uvicorn.access": {
+            "handlers": ["default", "file_handler"],
+            "level": settings.logs.uvicorn_log_level,
+            "propagate": False,
+        },
+        "uvicorn.error": {
+            "handlers": ["default", "file_handler"],
+            "level": settings.logs.uvicorn_log_level,
+            "propagate": False,
+        },
+        "uvicorn.asgi": {
+            "handlers": ["stream_handler", "file_handler"],
+            "level": settings.logs.uvicorn_log_level,
+            "propagate": False,
+        },
+    },
+}
--- backend/run.py
+++ backend/run.py
@@ -2,9 +2,10 @@ import uvicorn
 
 from app import main_app
 from app.config import settings
+from app.uvicorn_log_settings import LOGGING_CONFIG
 
 __all__ = ["main_app"]
 
 if __name__ == "__main__":
     # Запуск сервера
-    uvicorn.run("run:main_app", host="0.0.0.0", port=8000, reload=settings.reload)
+    uvicorn.run("run:main_app", host="0.0.0.0", port=8000, reload=settings.reload, log_config=LOGGING_CONFIG)
