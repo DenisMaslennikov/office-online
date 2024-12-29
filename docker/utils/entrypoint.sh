7dfca8a69bda381924b40d93f5c01802d0f590b3
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-29 11:40:24 +0300

    add rabbitmq

--- docker/utils/entrypoint.sh
+++ docker/utils/entrypoint.sh
@@ -8,6 +8,8 @@ readonly cmd="$*"
 
 : "${DATABASE_HOST:=db}"
 : "${DATABASE_PORT:=5432}"
+: "${RABBITMQ_HOST:=rabbitmq}"
+: "${RABBITMQ_PORT:=5672}"
 
 # We need this line to make sure that this container is started
 # after the one with postgres:
@@ -19,6 +21,14 @@ readonly cmd="$*"
 
 # It is also possible to wait for other services as well: redis, elastic, mongo
 echo "Postgres ${DATABASE_HOST}:${DATABASE_PORT} is up"
+
+/wait-for-it.sh \
+  --host="$RABBITMQ_HOST" \
+  --port="$RABBITMQ_PORT" \
+  --timeout=90 \
+  --strict
+
+echo "Rabbitmq ${RABBITMQ_HOST}:${RABBITMQ_PORT} is up"
 
 # Evaluating passed command (do not touch):
 # shellcheck disable=SC2086
