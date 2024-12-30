cb653f29fc845d55fa0234767a4dc05e14df4f5b
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-30 21:07:34 +0300

    add delete queue method

--- backend/app/rabbitmq/rabbitmq_client.py
+++ backend/app/rabbitmq/rabbitmq_client.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from uuid import UUID
 
 import aio_pika
@@ -33,7 +34,7 @@ class AIORabbiMQClient:
         self.connection = await aio_pika.connect_robust(
             host=self.host, port=self.port, login=self.login, password=self.password
         )
-        self.channel = self.connection.channel()
+        self.channel = await self.connection.channel()
 
     async def _declare_exchange(
         self,
@@ -44,39 +45,38 @@ class AIORabbiMQClient:
     ) -> AbstractExchange:
         """Создает обменник с заданными параметрами."""
         logger.debug(f"Создаю обменник {exchange_name}")
-        return await self.channel.declare_exchange(
+        self.exchanges[exchange_name] = await self.channel.declare_exchange(
             exchange_name,
             exchange_type,
             durable=durable,
             auto_delete=auto_delete,
         )
+        return self.exchanges[exchange_name]
 
-    async def publish(self, company_id: UUID, message: str, channel_id: UUID) -> None:
+    async def publish(self, company_id: UUID, channel_id: UUID, message: str) -> None:
         """Публикует сообщение в обменник."""
         exchange_name = settings.rabbitmq.exchange_name_template.format(company_id=company_id)
         routing_key = settings.rabbitmq.routing_key_template.format(channel_id=channel_id)
         try:
-            exchange = self.exchanges.setdefault(
-                exchange_name,
-                await self._declare_exchange(exchange_name, aio_pika.ExchangeType.TOPIC, durable=True),
-            )
+            exchange = self.exchanges.get(exchange_name)
+            if exchange is None:
+                exchange = await self._declare_exchange(exchange_name, aio_pika.ExchangeType.TOPIC, auto_delete=True)
             logger.debug(f"Публикую сообщение в {exchange_name}: {message}")
-            await exchange.publish(aio_pika.Message(message.encode("utf-8")), routing_key=routing_key)
+            await exchange.publish(aio_pika.Message(message), routing_key=routing_key)
         except aio_pika.exceptions.ChannelClosed:
             logger.debug("Обменник был удален. Создаю новый обменник.")
             exchange = await self._declare_exchange(exchange_name, aio_pika.ExchangeType.TOPIC, auto_delete=True)
-            self.exchanges[exchange_name] = exchange
-            await exchange.publish(aio_pika.Message(message.encode("utf-8")), routing_key=routing_key)
+            await exchange.publish(aio_pika.Message(message), routing_key=routing_key)
 
     async def update_channels_bindings(self, company_id: UUID, channel_id: UUID, user_id: UUID, action: str) -> None:
         """Обновляет подписки пользователя на канал."""
         exchange_name = settings.rabbitmq.exchange_name_template.format(company_id=company_id)
         routing_key = settings.rabbitmq.routing_key_template.format(channel_id=channel_id)
 
-        queue = await self.channel.declare_queue(
-            settings.rabbitmq.queue_name_template.format(user_id=user_id), exclusive=True
-        )
         exchange = await self._declare_exchange(exchange_name, aio_pika.ExchangeType.TOPIC, auto_delete=True)
+
+        queue_name = settings.rabbitmq.queue_name_template.format(user_id=user_id)
+        queue = await self.channel.declare_queue(queue_name, exclusive=True)
 
         logger.debug(
             f"Обновляю слушателя канала {channel_id} в компании {company_id} для пользователя {user_id} действие "
@@ -90,6 +90,15 @@ class AIORabbiMQClient:
         else:
             logger.error(f"Неизвестный тип действия {action}")
             raise ValueError(f"Недопустимый тип действия {action}")
+
+    async def delete_queue(self, user_id: UUID) -> None:
+        """Удаление очереди."""
+        queue_name = settings.rabbitmq.queue_name_template.format(user_id=user_id)
+        queue = await self.channel.declare_queue(queue_name, exclusive=True)
+        logger.debug(f"Отчищаю очередь для пользователя {user_id}")
+        await queue.purge()
+        logger.debug(f"Удаляю очередь пользователя {user_id}")
+        await queue.delete()
 
     async def close(self):
         """Закрытие подключения."""
