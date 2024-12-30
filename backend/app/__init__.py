9dc1ebf1b41ba9c163de5dd8077f5232a0d9c472
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-30 14:20:02 +0300

    add initialization connection to rabbitmq

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,14 +5,21 @@ from fastapi import FastAPI
 from app.api import api_router
 from app.config import settings
 from app.db import db_helper
+from app.logger import logger
+from app.rabbitmq import rabbitmq_client
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> None:
     """Жизненный цикл приложения."""
+    logger.debug("Инициализация FastAPI приложения")
+    await rabbitmq_client.connect()
+
     yield
 
+    logger.debug("Закрытие FasAPI приложения")
     await db_helper.dispose()
+    await rabbitmq_client.close()
 
 
 main_app = FastAPI(lifespan=lifespan)
