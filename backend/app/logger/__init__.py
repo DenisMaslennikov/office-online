b3fdb1873e069e25370f9b5ec53f3847b39d8334
Denis Maslennikov <ktotom7@yandex.ru>
2024-12-30 14:09:54 +0300

    fix logger

--- backend/app/logger/__init__.py
+++ backend/app/logger/__init__.py
@@ -4,16 +4,18 @@ from logging.handlers import RotatingFileHandler
 from app.config import settings
 
 logger = logging.getLogger(__name__)
-logger.setLevel(settings.logs.log_level)
+logger.setLevel(settings.logs.app_log_level)
 
 file_handler = RotatingFileHandler(
-    settings.logs.log_file, maxBytes=settings.logs.max_bytes, backupCount=settings.logs.backup_count
+    settings.logs.app_log_file, maxBytes=settings.logs.app_max_bytes, backupCount=settings.logs.app_max_bytes
 )
 
 console_handler = logging.StreamHandler()
 
-file_handler.setFormatter(settings.logs.app_log_format)
-console_handler.setFormatter(settings.logs.app_log_format)
+formatter = logging.Formatter(settings.logs.app_log_format)
+
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
